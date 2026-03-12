487dafa533324b1084e05a642e8c5cad36cc61a7
abrahamsudharsonponraj-prog <abrahamsudharson.ponraj@vit.ac.in>
2026-03-12 12:22:19 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.student.math.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println("Sum = " + c.add(5,3));
+        System.out.println("Product = " + c.multiply(5,3));
+
+    }
+
+}
